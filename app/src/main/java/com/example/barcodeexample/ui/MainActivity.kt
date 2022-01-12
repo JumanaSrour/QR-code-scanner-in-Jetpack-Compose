6f9300bb708922525fe47f12facf3b80077dd250
JumanaSrour <joumana.sroor2000@gmail.com>
2022-01-12 20:14:40 +0200

    creating analyzer class where all logic comes in

--- app/src/main/java/com/example/barcodeexample/ui/MainActivity.kt
+++ app/src/main/java/com/example/barcodeexample/ui/MainActivity.kt
@@ -1,11 +1,19 @@
-package com.example.barcodeexample
+package com.example.barcodeexample.ui
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.example.barcodeexample.QrCodeAnalyzer
+import com.example.barcodeexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContent {
+            QrCodeScannerComposeTheme{
+
+            }
+        }
     }
 }
